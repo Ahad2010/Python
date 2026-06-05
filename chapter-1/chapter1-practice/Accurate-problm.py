abbc139ddaf399e5d7c38a7dfbaedadc756639b3
Ahad Noor <ahadnoor442@gmail.com>
2026-06-05 01:14:05 -0700

    Stories changes in voice

--- chapter-1/chapter1-practice/Accurate-problm.py
+++ chapter-1/chapter1-practice/Accurate-problm.py
@@ -7,7 +7,7 @@ async def speak(text):
     await communicate.save("output.mp3")
     os.system("start output.mp3")
 
-asyncio.run(speak("Hello! I am speaking with a natural voice My name is Ahad and i am full stack developer and python engineer."))
+asyncio.run(speak("Once upon a time, a clever fox lived near a large, green forest. One sunny morning, he was walking through the woods looking for something to eat, when a wonderful smell caught his attention. High up in the branches of an old oak tree, a black crow sat with a large, delicious piece of cheese in her beak."))
 
 
 # asyncio.run(speak("Hello! I am speaking with a natural voice My name is Comatoze and i am from porn hub."))
