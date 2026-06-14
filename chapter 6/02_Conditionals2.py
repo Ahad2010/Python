2dbe1cb15a1490044e7697f236a87e2aede836ce
Ahad Noor <ahadnoor442@gmail.com>
2026-06-14 00:14:06 -0700

    Conditional if elif else

--- chapter 6/02_Conditionals2.py
+++ chapter 6/02_Conditionals2.py
@@ -12,3 +12,37 @@ elif(a==0):
 else :
     print("you are not above the age of consent")
 print("End of the program")
+
+
+
+a = int(input("Enter your age: "))
+
+if(a>18):
+    print("you are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering invalid Negative age")
+
+elif(a==0):
+    print("Your are entering 0 which is not a valid age")
+else :
+    print("you are not above the age of consent")
+print("End of the program")
+
+
+
+a = int(input("Enter your age: "))
+
+if(a>18):
+    print("you are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering invalid Negative age")
+
+elif(a==0):
+    print("Your are entering 0 which is not a valid age")
+else :
+    print("you are not above the age of consent")
+print("End of the program")
