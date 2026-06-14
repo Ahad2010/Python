4d7a580efb285d67e8f6ca22012cd2802c745b0a
Ahad Noor <ahadnoor442@gmail.com>
2026-06-14 04:49:33 -0700

    Practice for Learning

---
+++ chapter-6-practice/02-problem.py
@@ -0,0 +1,13 @@
+kill = int(input("Apne kills dalo: "))
+Damage = int(input("Apna damage dalo: "))
+
+if kill>=10 and Damage>=2000 :
+    print("Bhai tu Pro player hai ")
+
+elif kill>=5 and Damage>=1000 :
+    print("Abhi practice kr Bache")
+
+elif kill>=2 and Damage>=500 :
+    print("Bhai to game delete kr dai")
+
+print("Thanks fo contacting")
---
+++ chapter-6-practice/03-problem.py
@@ -0,0 +1,12 @@
+Markes = int(input('Enter your Markes:')) 
+
+if Markes>90:
+    print("Excellent You are the topper")
+
+elif Markes>60:
+    print("Good Keep more hard work")
+
+elif Markes>33:
+    print("Aby to prhna chor Kam sekh lai")
+
+print("End of the program")
