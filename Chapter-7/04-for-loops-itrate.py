090f7af471382707871ddee85dba401fac606bda
Ahad Noor <ahadnoor442@gmail.com>
2026-06-14 22:02:51 -0700

    For loops in python

---
+++ Chapter-7/04-for-loops-itrate.py
@@ -0,0 +1,17 @@
+#For loops with lists 
+l = [1,4,6,234,6,764]
+
+for i in l:
+      print(i)
+
+#For loops with Tuples 
+
+t = (6,23,175,122)
+for i in t:
+    print(i)
+
+#For loops with String
+
+s  = "Ahad noor"
+for i in s :
+    print(i)
