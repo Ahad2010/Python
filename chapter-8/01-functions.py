dcf01b72c0dcdd073b2db33e551ae3f3e10447cf
Ahad Noor <ahadnoor442@gmail.com>
2026-06-18 03:44:56 -0700

    Funtions started

---
+++ chapter-8/01-functions.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your number: "))
+b = int(input("Enter your number: "))
+c = int(input("Enter your number: "))
+
+
+average =(a+b+c)/3
+print(average)
+
+a = 3
+b = 2
+c = 1
+average =(a+b+c)/3
+print(average)
