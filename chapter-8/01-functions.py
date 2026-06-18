9d887ab12d1c3ad9c3272a39260bf9196cad1bb3
Ahad Noor <ahadnoor442@gmail.com>
2026-06-18 03:53:52 -0700

    Funcitons practice

--- chapter-8/01-functions.py
+++ chapter-8/01-functions.py
@@ -1,13 +1,30 @@
-a = int(input("Enter your number: "))
-b = int(input("Enter your number: "))
-c = int(input("Enter your number: "))
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
 
 
-average =(a+b+c)/3
-print(average)
+# average =(a+b+c)/3
+# print(average)
 
-a = 3
-b = 2
-c = 1
-average =(a+b+c)/3
-print(average)
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+# average =(a+b+c)/3
+# print(average)
+
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    
+    average =(a+b+c)/3
+    print(average)
+
+avg()
+print("Thankyou!")
+avg() 
+print("Thankyou!")  
+avg() 
+print("Thankyou!")  
+avg() 
+avg()   
