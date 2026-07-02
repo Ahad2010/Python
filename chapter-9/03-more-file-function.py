c96da7d4cab2abfc6050850ca72d5b4cc26732a3
Ahad Noor <ahadnoor442@gmail.com>
2026-07-02 04:15:57 -0700

    Lines in python

--- chapter-9/03-more-file-function.py
+++ chapter-9/03-more-file-function.py
@@ -1,6 +1,15 @@
 f = open("chapter-9/f.txt")
 lines = f.readlines()
-
 print(lines , type(lines))
+
+lines1 = f.readlines()
+print(lines1 , type(lines1))
+lines2 = f.readlines()
+print(lines2 , type(lines2))
+lines3 = f.readlines()
+print(lines3 , type(lines3))
+
+
+
 
 f.close()
